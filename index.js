eaeff349c3c4c969acc7f1012b5eb6bc272f2964
Abbos Soatmurodov <soatmurodovabbos@gmail.com>
2025-12-01 03:14:41 +0500

    feat: streamline property HTML generation by simplifying code structure

--- index.js
+++ index.js
@@ -2,25 +2,18 @@ import placeholderPropertyObj from "./properties/placeholderPropertyObj.js";
 import propertyForSaleArr from "./properties/propertyForSaleArr.js";
 
 function getPropertyHtml(arr = [placeholderPropertyObj]) {
-  return arr
-    .map((item) => {
+  return arr.map((item) => {
       const { propertyLocation, priceGBP, roomsM2, comment, image } = item;
-      const sumOfRoomsSize = roomsM2.reduce(
-        (total, currentRoom) => total + currentRoom
-      );
-      return `
-        <section class="card">
-				<img src="/images/${image}" alt="house for sold">
+      const sumOfRoomsSize = roomsM2.reduce((total, currentRoom) => total + currentRoom);
+      return `<section class="card">
+				<img src="./images/${image}" alt="house for sold">
 				<div class="card-right">
 					<h2>${propertyLocation}</h2>
 					<h3>${priceGBP} GBP</h3>
 					<p>${comment}</p>
 					<h3>${sumOfRoomsSize} m&sup2</h3>
 				</div>
-		</section>
-    `;
-    })
-    .join(" ");
+		</section>`}).join(" ");
 }
 
 document.getElementById("container").innerHTML = getPropertyHtml(propertyForSaleArr);
