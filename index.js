826f5d8cc61ee4cb41ff4a1f03d93ca4e0149263
Abbos Soatmurodov <soatmurodovabbos@gmail.com>
2025-12-01 02:54:25 +0500

    feat: simplify property card HTML generation and optimize room size calculation

--- index.js
+++ index.js
@@ -2,30 +2,25 @@ import placeholderPropertyObj from "./properties/placeholderPropertyObj.js";
 import propertyForSaleArr from "./properties/propertyForSaleArr.js";
 
 function getPropertyHtml(arr = [placeholderPropertyObj]) {
-  let htmlContent = [];
-
-  function sumOfRoomsSize(obj) {
-    return obj.reduce((total, currentRoom) => total + currentRoom);
-  }
-
-  arr.map((item) => cardMaker(item));
-
-  function cardMaker(obj) {
-    const { propertyLocation, priceGBP, roomsM2, comment, image } = obj;
-    htmlContent.push(`
+  return arr
+    .map((item) => {
+      const { propertyLocation, priceGBP, roomsM2, comment, image } = item;
+      const sumOfRoomsSize = roomsM2.reduce(
+        (total, currentRoom) => total + currentRoom
+      );
+      return `
         <section class="card">
 				<img src="/images/${image}" alt="house for sold">
 				<div class="card-right">
 					<h2>${propertyLocation}</h2>
 					<h3>${priceGBP} GBP</h3>
 					<p>${comment}</p>
-					<h3>${sumOfRoomsSize(roomsM2)} m&sup2</h3>
+					<h3>${sumOfRoomsSize} m&sup2</h3>
 				</div>
 		</section>
-    `);
-  }
-
-  return htmlContent.join(" ");
+    `;
+    })
+    .join(" ");
 }
 
 document.getElementById("container").innerHTML = getPropertyHtml(propertyForSaleArr);
